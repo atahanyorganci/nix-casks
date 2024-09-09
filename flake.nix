1f3e1ad9aff8754d8aa98590897c6de98906597e
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-09 20:02:43 +0200

    feat: add WhatsApp cask definition as a package

--- flake.nix
+++ flake.nix
@@ -7,8 +7,13 @@
     let
       systems = nixpkgs.lib.platforms.all;
       eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+      darwin = nixpkgs.lib.platforms.darwin;
+      eachDarwinSystem = f: nixpkgs.lib.genAttrs darwin (system: f nixpkgs.legacyPackages.${system});
     in
     {
       formatter = eachSystem (pkgs: pkgs.nixpkgs-fmt);
+      packages = eachDarwinSystem (pkgs: rec {
+        whatsapp = pkgs.callPackage ./packages/whatsapp { };
+      });
     };
 }
