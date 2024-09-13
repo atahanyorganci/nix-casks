d0efd8da52d8de92204ceb79f73812f3d520f5c9
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-13 12:47:30 +0200

    refactor: reimplement `unpackPhase` to be more robust
    For installing from .dmg files now `hdiutil` is used to attach the image and copy the contents. Previously `undmg` was used which cannot handle APFS images.

--- packages/default.nix
+++ packages/default.nix
@@ -1,28 +1,67 @@
 { stdenv, pkgs, fetchurl, lib, cask, ... }:
 let
+  installDmg = ''
+    mnt=$(mktemp -d -t ci-XXXXXXXXXX)
+
+    function finish {
+      echo "Detaching $mnt"
+      /usr/bin/hdiutil detach $mnt -force
+      rm -rf $mnt
+    }
+    trap finish EXIT
+
+    echo "Attaching $mnt"
+    /usr/bin/hdiutil attach -nobrowse -readonly $src -mountpoint $mnt
+
+    echo "Contents of $mnt"
+    ls -la $mnt/
+
+    echo "Copying contents"
+    shopt -s extglob
+    DEST="$PWD"
+    (cd "$mnt"; cp -a !(Applications) "$DEST/")
+    echo "Contents copied to $DEST"
+  '';
+  installZip = ''
+    tmp=$(mktemp -d -t ci-XXXXXXXXXX)
+    unzip -d $tmp $src
+    DEST="$PWD"
+    cd $tmp && cp -a . "$DEST" && cd $DEST
+  '';
   artifact = (builtins.elemAt (builtins.filter (entry: entry.type == "app") cask.artifacts) 0).value;
+  appCopy = artifact: ''
+    mkdir -p "$out/Applications" && cp -r "${artifact.name}" "$out/Applications/${artifact.target or artifact.name}"
+  '';
+  binaryLink = binary:
+    let
+      src = builtins.replaceStrings [ "$APPDIR" ] [ "$out/Applications" ] "${binary}";
+      parts = lib.splitString "/" binary;
+      name = builtins.elemAt parts (builtins.length parts - 1);
+    in
+    "mkdir -p $out/bin && ln -s ${src} $out/bin/${name}";
+  installScript = builtins.concatStringsSep "\n" (builtins.map ({ type, value }: if type == "app" then (appCopy value) else if type == "binary" then (binaryLink value) else "") cask.artifacts);
 in
 stdenv.mkDerivation rec {
   inherit (cask) pname version;
-  buildInputs = with pkgs; [ file unzip undmg ];
-  phases = [ "unpackPhase" "installPhase" ];
-  unpackPhase = ''
-    mkdir -p $out/Applications
-    type=$(file -b --mime-type $src)
-    if [ "$type" = "application/zip" ]; then
-      unzip -d $out/Applications $src
-    elif [ "$type" = "application/x-apple-diskimage" ] || [ "$type" = "application/x-bzip2" ]; then
-      undmg $src
-      cp -r ${artifact.name} $out/Applications/${artifact.target or artifact.name}
+  nativeBuildInputs = with pkgs; [ file unzip undmg ];
+  unpackPhase =
+    if lib.strings.hasSuffix ".dmg" cask.src.name then installDmg
+    else if lib.strings.hasSuffix ".zip" cask.src.name then installZip
     else
-      echo "Unsupported file type: $type"
-      exit 1
-    fi
+      ''
+        type=$(file -b --mime-type $src)
+        if [ "$type" = "application/zip" ]; then
+          ${installZip}
+        else
+          echo "Unsupported file type: $type"
+          exit 1
+        fi
+      '';
+  installPhase = ''
+    ${installScript}
+    echo '${installScript}' > $out/install.sh
   '';
-  src = fetchurl {
-    inherit (cask.src) url sha256;
-    name = "${pname}-${version}";
-  };
+  src = fetchurl cask.src;
   meta = with stdenv.lib; {
     inherit (cask.meta) description homepage;
     platforms = lib.platforms.darwin;
