d0b169edca98878f38255b06a0950b62d361bb0a
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-23 22:31:24 +0200

    feat: integrate Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind()],
 });
