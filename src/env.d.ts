bd35b989bd97db188f8cf0a5f465a4a0885ea71a
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-24 18:29:39 +0200

    feat: add D1 binding

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type D1Database = import("@cloudflare/workers-types").D1Database;
+
+type ENV = {
+  DB: D1Database;
+};
 
 declare namespace App {
   interface Locals extends Runtime {}
