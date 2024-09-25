faedd7abd4cf20e66677b24152cf897245ebe3c8
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-26 01:06:31 +0200

    refactor: rename and export schemas related to package identifiers

--- src/lib/package.ts
+++ src/lib/package.ts
@@ -3,26 +3,38 @@ import { and, desc, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
 import { caskPackages } from "../schema";
 
-const name = z
+/**
+ * Package name is a string that contains only lowercase letters, numbers, and hyphens.
+ */
+export const PkgName = z
   .string()
   .regex(/^[a-z0-9-]+$/)
   .transform(value => ({ name: value }));
+export type PkgName = z.infer<typeof PkgName>;
 
-const nameVersion = z
+/**
+ * Package name with version is a string that contains a package name and a version number separated by a hyphen.
+ */
+export const PkgNameVersion = z
   .string()
   .regex(/^[a-z0-9-]+-[0-9.]+$/)
   .transform(value => {
     const [name, version] = value.split("-");
     return { name, version };
   });
+export type PkgNameVersion = z.infer<typeof PkgNameVersion>;
 
-const nameVersionHash = z
+/**
+ * Package name with version and hash is a string that contains a package name, a version number, and a SHA256 hash
+ */
+export const PkgNameVersionHash = z
   .string()
   .regex(/^[a-z0-9-]+-[0-9.]+-[A-Za-z0-9+/]+={0,2}$/)
   .transform(value => {
     const [name, version, hash] = value.split("-");
     return { name, version, hash };
   });
+export type PkgNameVersionHash = z.infer<typeof PkgNameVersionHash>;
 
 /**
  * A package identifier can be one of the following:
@@ -30,8 +42,8 @@ const nameVersionHash = z
  * - `name-version`: a specific version of a package
  * - `name-version-hash`: a specific version of a package with SHA256 hash
  */
-export const identifier = z.union([name, nameVersion, nameVersionHash]);
-export type Identifier = z.infer<typeof identifier>;
+export const PkgIdentifier = z.union([PkgName, PkgNameVersion, PkgNameVersionHash]);
+export type PkgIdentifier = z.infer<typeof PkgIdentifier>;
 
 /**
  * Find a package by its identifier. If the identifier has hash, it will use unique index on hash.
@@ -42,7 +54,7 @@ export type Identifier = z.infer<typeof identifier>;
  * @param identifier package identifier
  * @returns package
  */
-export async function findPackageByIdentifier(db: D1Database, identifier: Identifier) {
+export async function findPackageByIdentifier(db: D1Database, identifier: PkgIdentifier) {
   const d = drizzle(db, {
     schema: { caskPackages },
   });
