fbe640f7f8918d55ed072dcccb51640c9338aa55
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-10-27 20:50:55 +0300

    feat: create route for creating packages

--- src/pages/api/[...route]/index.ts
+++ src/pages/api/[...route]/index.ts
@@ -1,10 +1,12 @@
+import { zValidator } from "@hono/zod-validator";
 import type { APIContext } from "astro";
 import { z } from "astro/zod";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
-import { createDatabase, type Database } from "~/db";
-import { createApiKey } from "~/lib/apikey";
+import { caskPackages, createDatabase, type Database } from "~/db";
+import { createApiKey, verifyApiKey } from "~/lib/apikey";
+import { fetchCaskFromUrl } from "~/lib/fetch";
 import { findPackageByIdentifier, PkgIdentifier } from "~/lib/package";
 
 const app = new Hono<{
@@ -24,6 +26,52 @@ app.get("/cask", async c => {
   const records = await c.env.DB.query.caskPackages.findMany();
   return c.json(records);
 });
+
+function unauthorized(message?: string): never {
+  throw new HTTPException(401, {
+    message: message || "Unauthorized",
+  });
+}
+
+app.use("*", async (c, next) => {
+  if (c.req.method === "POST") {
+    const apiKey = c.req.header("x-api-key") ?? unauthorized("API key required");
+    const verified = await verifyApiKey(c.env.DB, apiKey);
+    if (!verified) {
+      unauthorized("Invalid API key");
+    }
+  }
+  return await next();
+});
+
+app.post(
+  "/cask/new",
+  zValidator(
+    "json",
+    z.object({
+      url: z.string().url(),
+    }),
+  ),
+  async c => {
+    const { url } = c.req.valid("json");
+    const {
+      pname,
+      hash,
+      nix: { artifacts, ...nix },
+      version,
+    } = await fetchCaskFromUrl(url);
+    const persistedPackage = await c.env.DB.insert(caskPackages)
+      .values({
+        pname,
+        hash,
+        version,
+        cask: nix,
+      })
+      .returning();
+
+    return c.json(persistedPackage);
+  },
+);
 
 app.get("/cask/:identifier", async c => {
   const result = z
