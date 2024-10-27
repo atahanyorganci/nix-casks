c451b004d46e7237371c28698e8e47e6ed5bad4e
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-10-27 20:35:24 +0300

    fix: remove unused import

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,5 @@
 import { sql } from "drizzle-orm";
 import { primaryKey, sqliteTable, text, uniqueIndex } from "drizzle-orm/sqlite-core";
-import { createDatabase } from "~/db";
 
 export const caskPackages = sqliteTable(
   "cask_packages",
