c5b93a9d87ed916269cdddc5ad6bbcff07448e15
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-11-04 04:34:42 +0300

    refactor: migrate `/cask` routes use `@hono/zod-openapi`

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,88 +1,23 @@
-import { OpenAPIHono, z } from "@hono/zod-openapi";
-import { zValidator } from "@hono/zod-validator";
-import { HTTPException } from "hono/http-exception";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { logger } from "hono/logger";
-import { verifyApiKey } from "~/lib/apikey";
-import { fetchCaskFromUrl } from "~/lib/fetch";
-import { findPackageByIdentifier, PkgIdentifier } from "~/lib/package";
-import { packages } from "~/server/db";
 import apikeyRouter from "./routers/apikey";
+import packagesRouter from "./routers/package";
 import type { AppContext } from "./types";
 
 export const app = new OpenAPIHono<AppContext>().basePath("/api");
 
+// Register middlewares
 app.use(logger());
+
+// Register routers
 app.route("/apikey", apikeyRouter);
+app.route("/package", packagesRouter);
+
+// Register OpenAPI documentation
 app.doc("/doc", {
   openapi: "3.0.0",
   info: {
     version: "0.0.1",
     title: "Nix Casks Documentation",
   },
-});
-
-app.get("/cask", async c => {
-  const records = await c.env.DB.query.packages.findMany();
-  return c.json(records);
-});
-
-function unauthorized(message?: string): never {
-  throw new HTTPException(401, {
-    message: message || "Unauthorized",
-  });
-}
-
-app.use("*", async (c, next) => {
-  if (c.req.method === "POST") {
-    const apiKey = c.req.header("x-api-key") ?? unauthorized("API key required");
-    const verified = await verifyApiKey(c.env.DB, apiKey);
-    if (!verified) {
-      unauthorized("Invalid API key");
-    }
-  }
-  return await next();
-});
-
-app.post(
-  "/cask/new",
-  zValidator(
-    "json",
-    z.object({
-      url: z.string().url(),
-    }),
-  ),
-  async c => {
-    const { url } = c.req.valid("json");
-    const {
-      pname,
-      hash,
-      nix: { artifacts, ...nix },
-      version,
-    } = await fetchCaskFromUrl(url);
-    const persistedPackage = await c.env.DB.insert(packages)
-      .values({ pname, hash, version, nix })
-      .returning();
-
-    return c.json(persistedPackage);
-  },
-);
-
-app.get("/cask/:identifier", async c => {
-  const result = z
-    .object({
-      identifier: PkgIdentifier,
-    })
-    .safeParse(c.req.param());
-  if (!result.success) {
-    throw new HTTPException(400, {
-      message: "Invalid package identifier",
-    });
-  }
-  const cask = await findPackageByIdentifier(c.env.DB, result.data.identifier);
-  if (!cask) {
-    throw new HTTPException(404, {
-      message: "Package not found",
-    });
-  }
-  return c.json(cask);
 });
---
+++ src/server/api/routers/package.ts
@@ -0,0 +1,131 @@
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+import { fetchCaskFromUrl } from "~/lib/fetch";
+import { findPackageByIdentifier, Package, PackageNameVersionHash } from "~/lib/package";
+import { packages } from "~/server/db";
+import { type AppContext } from "../types";
+
+const packagesRouter = new OpenAPIHono<AppContext>();
+
+packagesRouter.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    description: "Get latest version of all packages",
+    responses: {
+      200: {
+        description: "List of packages",
+        content: {
+          "application/json": {
+            schema: Package.array().openapi({
+              description: "List of packages",
+            }),
+          },
+        },
+      },
+    },
+  }),
+  async c => {
+    const records = await c.env.DB.select({
+      pname: packages.pname,
+      hash: packages.hash,
+      version: packages.version,
+      nix: packages.nix,
+    }).from(packages);
+    return c.json(records, 200);
+  },
+);
+
+packagesRouter.openapi(
+  createRoute({
+    method: "post",
+    path: "/",
+    description: "Add a new package",
+    request: {
+      body: {
+        content: {
+          "application/json": {
+            schema: z
+              .object({
+                url: z.string().url().openapi({
+                  description: "URL of the package to add",
+                  example: "https://formulae.brew.sh/api/cask/visual-studio-code.json",
+                }),
+              })
+              .openapi("CreatePackage"),
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "Package added",
+        content: {
+          "application/json": {
+            schema: Package,
+          },
+        },
+      },
+    },
+  }),
+  async c => {
+    const { url } = c.req.valid("json");
+    const {
+      pname,
+      hash,
+      nix: { artifacts, ...nix },
+      version,
+    } = await fetchCaskFromUrl(url);
+    const [{ createdAt, ...persistedPackage }] = await c.env.DB.insert(packages)
+      .values({ pname, hash, version, nix })
+      .returning();
+    return c.json(persistedPackage);
+  },
+);
+
+packagesRouter.openapi(
+  createRoute({
+    method: "get",
+    path: "/{identifier}",
+    description: "Get a package by its identifier",
+    request: {
+      params: z
+        .object({
+          identifier: PackageNameVersionHash,
+        })
+        .openapi("GetPackageParams"),
+    },
+    responses: {
+      200: {
+        description: "Package found",
+        content: {
+          "application/json": {
+            schema: Package,
+          },
+        },
+      },
+      404: {
+        description: "Package not found",
+        content: {
+          "application/json": {
+            schema: z.object({
+              message: z.string().openapi({
+                description: "Error message",
+                example: "Package not found",
+              }),
+            }),
+          },
+        },
+      },
+    },
+  }),
+  async c => {
+    const { identifier } = c.req.valid("param");
+    const record = await findPackageByIdentifier(c.env.DB, identifier);
+    if (!record) {
+      return c.json({ message: "Package not found" }, 404);
+    }
+    return c.json(record, 200);
+  },
+);
+
+export default packagesRouter;
