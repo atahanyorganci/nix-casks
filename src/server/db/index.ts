99fbe4efce82502d71d964efee5e8ec4f1f37de0
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-11-19 15:19:14 +0300

    feat: add logger for Drizzle

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,11 +1,22 @@
+import type { Logger } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/vercel-postgres";
+import { createLogger } from "../logger";
 import * as schema from "./schema";
 
 export * from "./schema";
 
+class DrizzleLogger implements Logger {
+	private readonly logger = createLogger();
+
+	logQuery(query: string, params: unknown[]): void {
+		this.logger.info("Executing query", { query, params });
+	}
+}
+
 export function createDatabase() {
 	return drizzle({
 		schema,
+		logger: new DrizzleLogger(),
 	});
 }
 
