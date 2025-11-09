8ae94f4524b49bb16b327e4c6f87538660aa23cf
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-11-09 11:23:11 +0300

    refactor: start transitioning into `flake-parts`

--- flake.nix
+++ flake.nix
@@ -2,74 +2,25 @@
   description = "Nix flake port of Homebrew casks";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { nixpkgs, ... }:
-    let
-      systems = nixpkgs.lib.platforms.all;
-      eachSystem = f: nixpkgs.lib.genAttrs systems (system:
-        let
-          pkgs = import nixpkgs {
+  outputs = inputs@{ flake-parts, self, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ];
+        perSystem = { system, ... }: {
+          _module.args.pkgs = import self.inputs.nixpkgs {
             inherit system;
             config = {
               allowUnfree = true;
+              allowBroken = true;
             };
           };
-        in
-        f pkgs
-      );
-      darwin = nixpkgs.lib.platforms.darwin;
-      eachDarwinSystem = f: nixpkgs.lib.genAttrs darwin (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      formatter = eachSystem (pkgs: pkgs.nixpkgs-fmt);
-      packages = eachDarwinSystem (pkgs:
-        let
-          archive = builtins.fromJSON (builtins.readFile ./archive.json);
-          source = pkgs.fetchurl {
-            inherit (archive) url sha256;
-            name = "packages.json";
-          };
-          json = builtins.fromJSON (builtins.readFile source);
-          packages = builtins.listToAttrs (map
-            (pkg: {
-              name = pkg.pname;
-              value = pkgs.callPackage ./packages { inherit pkg; };
-            })
-            json);
-        in
-        packages);
-      devShells = eachSystem (pkgs:
-        let
-          archive = "./archive.json";
-          endpoint = "https://nix-casks.yorganci.dev/api/archive/latest";
-          ci = pkgs.writeShellScriptBin "ci" ''
-            set -e
-            curl -o "${archive}" -L --retry 5 --retry-delay 2 --fail "${endpoint}"
-          '';
-          packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-          packageManager = builtins.elemAt (builtins.split "\\+" packageJson.packageManager) 0;
-          pnpm-shim = pkgs.writeShellScriptBin "pnpm" ''
-            exec ${pkgs.nodejs-slim}/bin/node ${pkgs.nodejs-slim}/bin/corepack pnpm "$@"
-          '';
-        in
-        {
-          default = pkgs.mkShell {
-            shellHook = ''
-              corepack install -g ${packageManager}
-            '';
-            buildInputs = with pkgs; [
-              pnpm-shim
-              nodejs-slim
-              just
-              process-compose
-              curl
-              git
-              ngrok
-              ci
-              magika
-              flyctl
-            ];
-          };
-        });
-    };
+        };
+      };
 }
