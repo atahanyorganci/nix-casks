dc41ab0cee4bbf53475ec649d1102c0cec85364a
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-11-09 12:08:49 +0300

    feat: add `treefmt-nix` to format all sources

--- flake.nix
+++ flake.nix
@@ -3,8 +3,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ flake-parts, self, ... }:
+  outputs =
+    inputs@{ flake-parts
+    , self
+    , treefmt-nix
+    , ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; }
       {
         systems = [
@@ -14,6 +21,7 @@
           "x86_64-linux"
         ];
         imports = [
+          treefmt-nix.flakeModule
           ./nix
         ];
         perSystem = { system, ... }: {
--- nix/default.nix
+++ nix/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./casks
     ./ci.nix
+    ./treefmt.nix
     ./shell.nix
   ];
 }
