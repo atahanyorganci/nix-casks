6bccf1a001858fe46bcfe475aa663781ad3ae900
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-11-21 00:48:25 +0300

    feat: add bzip2 support

--- nix/casks/unpack.sh
+++ nix/casks/unpack.sh
@@ -22,33 +22,29 @@ DESCRIPTION=$(echo "$OUTPUT" | jq -r '.description')
 
 echo "File $FILE_NAME is $DESCRIPTION ($MIME_TYPE)"
 
-case "$MIME_TYPE" in
-    "application/zip")
-        echo "Unzipping $src to $DEST"
-        unzip -d "$TEMP" "$src"
-        echo "Contents of $TEMP:"
-        ls -l "$TEMP"
-        echo "Copying $TEMP to $DEST"
-        cd "$TEMP" && cp -a . "$DEST" && cd "$DEST" || exit 1
-        ;;
-    "application/x-bzip2")
-        echo "Unpacking $src to $DEST"
-        bzip2 -d "$TEMP" "$src"
-        echo "Contents of $TEMP:"
-        ls -l "$TEMP"
-        echo "Copying $TEMP to $DEST"
-        cd "$TEMP" && cp -a . "$DEST" && cd "$DEST" || exit 1
-        ;;
-    "application/x-apple-diskimage")
-        echo "Attaching $TEMP"
-        echo "Y" | /usr/bin/hdiutil attach -nobrowse -readonly "$src" -mountpoint "$TEMP"
-        MOUNTED=1
-        echo "Contents of $TEMP:"
-        ls -l "$TEMP"
-        echo "Copying $TEMP to $DEST"
-        cd "$TEMP" && cp -a !(Applications) "$DEST/" || exit 1
-        ;;
-    *)
-        echo "Unknown type $TYPE"
-        ;;
-esac
+if [ "$MIME_TYPE" = "application/zip" ]; then
+    echo "Unzipping $src to $DEST"
+    unzip -d "$TEMP" "$src"
+    echo "Contents of $TEMP:"
+    ls -l "$TEMP"
+    echo "Copying $TEMP to $DEST"
+    cd "$TEMP" && cp -a . "$DEST" && cd "$DEST" || exit 1
+elif [ "$MIME_TYPE" = "application/x-apple-diskimage" ]; then
+    echo "Attaching $TEMP"
+    echo "Y" | /usr/bin/hdiutil attach -nobrowse -readonly "$src" -mountpoint "$TEMP"
+    MOUNTED=1
+    echo "Contents of $TEMP:"
+    ls -l "$TEMP"
+    echo "Copying $TEMP to $DEST"
+    cd "$TEMP" && cp -a !(Applications) "$DEST/" || exit 1
+elif [ "$MIME_TYPE" = "application/x-bzip2" ]; then
+    echo "Unpacking $src to $DEST"
+    tar -xjf "$src" -C "$TEMP"
+    echo "Contents of $TEMP:"
+    ls -l "$TEMP"
+    echo "Copying $TEMP to $DEST"
+    cd "$TEMP" && cp -a . "$DEST" && cd "$DEST" || exit 1
+else
+    echo "Unknown type $TYPE"
+    exit 1
+fi
