3bf170328c7b654a972f9519fbc59473da8d7706
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-01-07 13:22:16 +0300

    refactor: reinitialize migrations for migration to PlanetScale Postgres

--- drizzle/0003_fast_mister_sinister.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "packages" DROP COLUMN "semver";
--- Drop `try_cast_to_semver`
-DROP FUNCTION IF EXISTS try_cast_to_semver(TEXT);
--- Drop the `semver` extension
-DROP EXTENSION IF EXISTS "semver";
